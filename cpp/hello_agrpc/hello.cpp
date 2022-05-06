9348a9a2d0b32d7d7bd0a5a0275b0c9e10d56ae9
codingfun2022 <codingfun2022@163.com>
2022-05-06 20:17:39 +0800

    Test search_path.

--- cpp/hello_agrpc/hello.cpp
+++ cpp/hello_agrpc/hello.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
 #include <boost/dll/runtime_symbol_info.hpp>
+#include <boost/process/search_path.hpp>
 
 int main()
 {
     std::cout << boost::dll::program_location() << std::endl;
-    auto p = boost::dll::program_location().parent_path() / "Makefile";
-    std::cout << p << ": " << boost::filesystem::is_regular_file(p) << std::endl;
+    auto mkf = boost::dll::program_location().parent_path() / "Makefile";
+    std::cout << mkf << ": " << boost::filesystem::is_regular_file(mkf) << std::endl;
+
+    auto py_path = boost::process::search_path("python");
+    std::cout << py_path << std::endl;
 }
