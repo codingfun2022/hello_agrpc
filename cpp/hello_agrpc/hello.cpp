0a08ad61f4846252501d411e110c608089d533fc
codingfun2022 <codingfun2022@163.com>
2022-05-06 19:14:49 +0800

    Test parent_path.

--- cpp/hello_agrpc/hello.cpp
+++ cpp/hello_agrpc/hello.cpp
@@ -4,4 +4,6 @@
 int main()
 {
     std::cout << boost::dll::program_location() << std::endl;
+    auto p = boost::dll::program_location().parent_path() / "Makefile";
+    std::cout << p << ": " << boost::filesystem::is_regular_file(p) << std::endl;
 }
