6b4e46858d6b22101d3fc6c5daf1ed83655dc040
codingfun2022 <codingfun2022@163.com>
2022-05-07 17:22:02 +0800

    Test STL set equal.

--- cpp/hello_agrpc/hello.cpp
+++ cpp/hello_agrpc/hello.cpp
@@ -6,7 +6,12 @@
 #include <boost/uuid/uuid_generators.hpp>
 #include <boost/uuid/uuid_io.hpp>
 
-int main()
+#include <iostream>
+#include <string>
+#include <vector>
+#include <unordered_set>
+
+void test_boost()
 {
     std::cout << boost::dll::program_location() << std::endl;
     auto mkf = boost::dll::program_location().parent_path() / "Makefile";
@@ -18,4 +23,18 @@ int main()
     boost::uuids::uuid uuid = boost::uuids::random_generator()();
     std::cout << uuid << std::endl;
     std::cout << boost::uuids::to_string(uuid) << std::endl;
+}
+
+void test_stl()
+{
+    std::vector<std::string> v1{"a", "b", "c"};
+    std::vector<std::string> v2{"b", "a", "a"};
+    std::unordered_set<std::string> s1(v1.begin(), v1.end());
+    std::unordered_set<std::string> s2(v2.begin(), v2.end());
+    std::cout << (s1 == s2) << std::endl;
+}
+
+int main()
+{
+    test_stl();
 }
